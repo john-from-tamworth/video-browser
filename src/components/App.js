4445faca9e38be40003800533fb39c9199feaf13
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-10-27 01:48:13 +0000

    added search bar margin

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,9 @@ class App extends React.Component {
   render() {
     return (
       <div className='ui container'>
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div style={{ marginTop: '10px', marginBottom: '10px' }}>
+          <SearchBar onFormSubmit={this.onTermSubmit} />
+        </div>
         <div className='ui grid'>
           <div className='ui row'>
             <div className='eleven wide column'>
